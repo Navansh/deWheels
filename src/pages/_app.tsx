3fd89f0b1de9a7683e27d2fc96692b7c2c1a64e9
Pjijin <pjijin1@gmail.com>
2023-07-25 18:45:15 +0530

    🐛 Fix: updated ui components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,35 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import '@rainbow-me/rainbowkit/styles.css';
+import Header from '@/components/header';
+import '@/styles/globals.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient } = configureChains([polygonMumbai], [publicProvider()])
+const { chains, publicClient } = configureChains([polygonMumbai], [publicProvider()]);
 const { connectors } = getDefaultWallets({
-  appName: 'depool',
-  projectId: '9d76ee4cef7c5b8d16e72986446c7841',
-  chains
+	appName: 'deWheels',
+	projectId: '9d76ee4cef7c5b8d16e72986446c7841',
+	chains,
 });
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
+	autoConnect: true,
+	connectors,
+	publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  )
+	return (
+		<WagmiConfig config={wagmiConfig}>
+			<RainbowKitProvider chains={chains} modalSize="compact">
+				<Header>
+					<Component {...pageProps} />
+					<Toaster />
+				</Header>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
 }
